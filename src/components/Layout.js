88ca27de44356cd88f93e5190e15f21f496365e8
Ogbemudia Rex <rex.ogbemudia@andela.com>
2019-12-15 18:31:18 +0100

    add identity widget (#1)

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,20 +12,42 @@ function Layout(props) {
   const description = props.description || defaultDescription;
 
   return (
-    <div>
+    <>
+      <div>
+        <Helmet>
+          <html lang='en' />
+          <title>{title}</title>
+          <meta name='description' content={description} />
+
+          <meta name='theme-color' content='#fff' />
+
+          <meta property='og:type' content='business.business' />
+          <meta property='og:title' content={title} />
+          <meta property='og:url' content='/' />
+
+          <script
+            src='https://identity.netlify.com/v1/netlify-identity-widget.js'
+            type='text/javascript'
+          ></script>
+        </Helmet>
+        <main>{props.children}</main>
+      </div>
       <Helmet>
-        <html lang='en' />
-        <title>{title}</title>
-        <meta name='description' content={description} />
-
-        <meta name='theme-color' content='#fff' />
-
-        <meta property='og:type' content='business.business' />
-        <meta property='og:title' content={title} />
-        <meta property='og:url' content='/' />
+        <script type='text/javascript'>
+          {`
+            if (window.netlifyIdentity) {
+              window.netlifyIdentity.on("init", user => {
+                if (!user) {
+                  window.netlifyIdentity.on("login", () => {
+                    document.location.href = "/admin/";
+                  });
+                }
+              });
+            }
+        `}
+        </script>
       </Helmet>
-      <main>{props.children}</main>
-    </div>
+    </>
   );
 }
 
